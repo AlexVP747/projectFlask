60642935d1eaf0108625ba8f46711ba3646720bb
Alexey <a504512@mail.ru>
2023-12-11 21:25:30 +0300

    Подключение административной панели, создание функции поиска в БД, создание шаблонов

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from db.models import getALLGoods, getRangeGoods
+from flask import Flask, render_template, request, session, redirect
+from db.models import getALLGoods, getRangeGoods, getUser
 
 app = Flask(__name__)
 
@@ -19,6 +19,25 @@ def goods():
     minPrice = request.form["min"]
     maxPrice = request.form["max"]
     return render_template("goods.html", goods=getRangeGoods(minPrice, maxPrice))
+
+@app.route("/admin")
+def adminPage():
+  # Перенаправление пользователя, если он не зарегистрирован
+  if "login" not in session:
+    return redirect("/admin/login")
+  
+@app.route("/admin/login", methods=["GET", "POST"])
+def adminLoginPage():
+  if request.method == "GET":
+    return render_template("adminLogin.html")
+  elif request.method == "POST":
+    login = request.form["login"]
+    password = request.form["password"]
+
+
+
+
+
 
 app.run(debug=True)
 
--- db/models.py
+++ db/models.py
@@ -25,3 +25,19 @@ def getRangeGoods(minPrice, maxPrice):
   connection.close()
 
   return result
+
+# Создаем функцию поиска пользователя в базе данных
+def getUser(login, password):
+  connection = sqlite3.connect("base.db")
+  cursor = connection.cursor()
+
+  cursor.execute("""
+      SELECT *
+      FROM users
+      WHERE login = ? AND password = ?                                              
+  """, (login, password))
+  result = cursor.fetchone()
+
+  connection.close()
+
+  return result
